c60cf22af5f729c177ba31cf3df5a92f8be6863c
Michael Zech <michael@Michaels-Mini.fritz.box>
2019-09-01 08:04:16 +0200

    Removed the inline JavaScript within add.js.

--- js/add.js
+++ js/add.js
@@ -6,10 +6,14 @@ $(() => {
     if ($("#positions").children().length < 9) {
       $("#positions").append(`<div id="position${++positionNumber}">
         <p>Year: <input type="text" name="year${positionNumber}" value="">
-        <input type="button" value="-" onclick="$('#position${positionNumber}').remove();return false;"></p>
+        <input type="button" id="minusButton${positionNumber}" value="-"></p>
         <textarea name="desc${positionNumber}" rows="8" cols="80"></textarea>
         </div>
       </div>`);
+
+      $("#minusButton" + positionNumber).click(() => {
+        $("#position" + positionNumber).remove();
+      });
     } else {
       alert("Maximum of nine position entries exceeded");
     }
